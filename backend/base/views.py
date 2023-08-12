2a3b183111e71bd93c1e25e0ce1b03ece4017812
Aisha <aisha.zhumagul@alumni.nu.edu.kz>
2023-08-12 20:19:59 +0600

    created models and endpoints for categories and news

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from base.models import Category, News
+from base.serializers import CategorySerializer, NewsSerializer
 
-# Create your views here.
+
 def index(request):
     return render(request, 'index.html')
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def getCategories(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def getNews(request):
+    news = News.objects.all()
+    serializer = NewsSerializer(news, many=True)
+    return Response(serializer.data)
